14a4c8b038082f5e93257d8bf6cfa9d78e73207a
JNMedina32 <JNMedina32.github.io>
2023-04-22 15:56:52 -0400

    Add styles to form and saved recipes div

--- script.js
+++ script.js
@@ -7,15 +7,22 @@ const urlToFetch = `${baseURL}&${appID}&${appKey}`;
 
 //variables for dynamic html
 const resultContainer = document.querySelector('.resultContainer');
-const imgContainer = document.querySelector('.imgContainer');
-const firstImgTag = document.querySelector('.firstImg');
-
 const submitButton = document.querySelector('#submitBtn');
 
 //grabs the form key and values 
 const formEl = document.querySelector('#recipeFilters');
 
+//function to dynamically display to html
+function displayRecipes(hits){
+    hits.forEach(recipe => {
+        resultContainer.innerHTML += `
+        <h3>${recipe.recipe.label}</h3>
+        <img src=${recipe.recipe.images.SMALL.url}>
+        `;
+    })
+}
 
+//grabs filters then fetches
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     const paramArray = [];
@@ -55,28 +62,6 @@ submitButton.addEventListener('click', (e) => {
     submitBtn();
 });
 
-
-
-
-
-//Grabbing the radio input for meal type
-const mealType = document.getElementsByName('mealType');
-
-const mealChecked = () => {
-    for(var meal of mealType){
-        if(meal.checked) {
-            if(meal.value != 'Any'){
-                return meal.value
-            } else return;
-        }
-    }
-}
-//console.log(mealChecked());
-
-//grabing the dropdown selection for Dish Type
-const dishType = document.getElementById('dishType');
-//console.log(dishType);
-
 const getRecipeByDish = async (name) => {
     const specificDish = `q=${name}`;
     const urlToFetchByDish = `${baseURL}&${specificDish}&${appID}&${appKey}`;
@@ -93,27 +78,5 @@ const getRecipeByDish = async (name) => {
         console.log(error);
     };
 };
-function dropdownSelection(dropdown){
-    let dropdownList = dropdown;
-    let selectedIndex = dropdownList.selectedIndex;
-    let selectedOption = dropdownList.options[selectedIndex];
-    return selectedOption.value;
-} 
-//dropdownSelection(dishType);
-// async function submitBtn(){
 
-//     const param = `&dishType=${paramArray[0]}`;
-//     const fetchURL = `${urlToFetch}${param}`;
-//     try{
-//         const response = await fetch(fetchURL);
-//         if(response.ok){
-//             const jsonResponse = await response.json();
-//             const hits = jsonResponse.hits;
-//             const imgSrc = hits[0].recipe.images.THUMBNAIL.url;
-//             firstImgTag.setAttribute('src', imgSrc);
-//         }
-//     }catch(error){
-//         console.log(error);
-//     }
-// };
 // console.log(submitBtn())
