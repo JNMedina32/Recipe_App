2de254f79664af2b220951ac30416e76d89fedc2
JNMedina32 <JNMedina32.github.io>
2023-04-23 21:10:09 -0400

    90 percent done. Maybe add classes for saved container

--- script.js
+++ script.js
@@ -3,24 +3,60 @@ console.log('Running Recipe App')
 const baseURL = `https://api.edamam.com/api/recipes/v2?type=public`;
 const appID = ``;
 const appKey = ``;
-const urlToFetch = `${baseURL}&${appID}&${appKey}`;
+const urlToFetch = `${baseURL}${appID}${appKey}`;
 
 //variables for dynamic html
 const resultContainer = document.querySelector('.resultContainer');
 const submitButton = document.querySelector('#submitBtn');
+const filterContainer = document.querySelector('.filterContainer');
+const savedContainer = document.querySelector('.savedContainer');
+const userInput = document.querySelector('#userInput').value;
+const savedRecipes = document.querySelector('#savedRecipes');
 
-//grabs the form key and values 
+//grabs the form key and values pairs
 const formEl = document.querySelector('#recipeFilters');
 
+//saves recipes to the side and local storage 
+
+// savedRecipes.addEventListener('click', () => {
+//     savedContainer.innerHTML += `
+//     <div class='saves'>
+//     <img src=${hits.recipe.recipe.images.THUMBNAIL.url}>
+//     <h3>${hits.recipe.recipe.label}</h3>
+//     </div>
+//     `
+// })
+//saves recipes to the side and local storage 
+const saveRecipe = () => {
+    savedContainer.innerHTML += `
+    <div class='saves'>
+    <img src=${recipe.recipe.images.THUMBNAIL.url}>
+    <h3>${this.recipe.recipe.label}</h3>
+    </div>
+    `
+};
+
+
+
 //function to dynamically display to html
-function displayRecipes(hits){
+function displayRecipes(hits){ 
     hits.forEach(recipe => {
+        let calories = recipe.recipe.calories.toFixed(2);
         resultContainer.innerHTML += `
+        <div class='individualRecipe'>
         <h3>${recipe.recipe.label}</h3>
         <img src=${recipe.recipe.images.SMALL.url}>
-        `;
+        <p class='calories'><strong>Calories: ${calories} Time: ${recipe.recipe.totalTime}</strong></p>
+        <p>Ingredients: ${recipe.recipe.ingredientLines}</p>
+        </div>
+        <div class='prepAndSave'>
+        <a href="${recipe.recipe.url}" target='_blank' class='buttons'>How to Prepare</a> <div class='buttons' id='savedRecipes' onclick='saveRecipe()'>Save Recipe</div>
+        </div>
+        `;  
     })
-}
+};
+
+
 
 //grabs filters then fetches
 submitButton.addEventListener('click', (e) => {
@@ -28,31 +64,29 @@ submitButton.addEventListener('click', (e) => {
     const paramArray = [];
     for (let i = 0; i < formEl.elements.length; i++){
         var e = formEl.elements[i];
-        if(e.value != 'any'){
+        if(e.value != 'any' && e.value != ''){
         paramArray.push(encodeURIComponent(e.name) + "=" + encodeURIComponent(e.value));
         }
     };
+
     const queryStrings = paramArray.join('&');
-    console.log(queryStrings);
+
     async function submitBtn(){
-        const fetchURL = `${urlToFetch}&${queryStrings}`;
+        let fetchURL = `${urlToFetch}&${queryStrings}`;
+        if(userInput){
+            const userInputQueryString = `&q=${encodeURIComponent(userInput)}`;
+            fetchURL = `${baseURL}${userInputQueryString}${appID}${appKey}&${queryStrings}`
+        }
         try{
             const response = await fetch(fetchURL);
             if(response.ok){
                 const jsonResponse = await response.json();
                 const hits = jsonResponse.hits;
-                console.log(jsonResponse);
-                console.log(hits);
                 if(hits.length < 1){
                     resultContainer.innerHTML = `I apologize. Looks like there are no recipes that match your filters in this database <strong>:(</strong>`
                 }else {
                     resultContainer.innerHTML = ``;
-                    hits.forEach(recipe => {
-                        resultContainer.innerHTML += `
-                        <h3>${recipe.recipe.label}</h3>
-                        <img src=${recipe.recipe.images.SMALL.url}>
-                        `;
-                    })
+                    displayRecipes(hits);
                 }
             }
         }catch(error){
@@ -79,4 +113,3 @@ const getRecipeByDish = async (name) => {
     };
 };
 
-// console.log(submitBtn())
